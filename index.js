f01fd588cadf827bb9a757aa431309da8c4346c1
Eric Einerson <eaeiner.gmail.co>
2022-01-12 10:19:07 -0700

    commit tree 355cd766d38512c3f195a2ab16991903c854dfae

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arrayOfDrivers) => {
+    return arrayOfDrivers.slice(0,2);
+};
+
+//returns last two drivers in the array, not destructive
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+        return (fare) =>    {
+        console.log(multiplier);
+        console.log(fare);
+        return fare * multiplier;
+    };
+};
+console.log(createFareMultiplier());
+
+const fareDoubler = createFareMultiplier(2);
+//fareDoubler(10)
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers,selectDriversFunction){
+    console.log(selectDriversFunction);
+    return (selectDriversFunction(arrayOfDrivers));
+}
